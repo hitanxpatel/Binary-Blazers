10247568db3f598b27c1e34a69477bfa1b91f135
Sahil Mondal Shaikh <hshaikh1345@gmail.com>
2025-04-12 19:13:51 +0530

    final upload

---
+++ Ecommerce_Website_Project/Ecommerce_Website_Application/Staticfiles/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.querySelector(".logo");
+    const names = [
+        { text: "PACKS", color: "white" },
+        { text: "PACKAGING", color: "orange" },
+        { text: "AND", color: "orange" },
+        { text: "CONTROL", color: "white" },
+        { text: "KIT", color: "lime" },
+        { text: "SYSTEM", color: "lime" }
+    ];
+    let index = 0;
+
+    function changeLogoText() {
+        logo.textContent = names[index].text;
+        logo.style.color = names[index].color;
+        index = (index + 1) % names.length;
+    }
+
+    setInterval(changeLogoText, 2000); // Change every 2 seconds
+});
